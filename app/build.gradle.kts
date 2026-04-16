6e5fccf3896043ce91cd378c44814f8129f910ee
SrJhol <mendes.jhonatan039@gmail.com>
2026-04-15 21:11:29 -0300

    Ajustes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.jhonatan.prova_leo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jhonatan.prova_leo"
         minSdk = 25
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
